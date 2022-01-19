6e4f91b267fd88b9dd59cd63a1b3c89a7ed1ff3e
alexnicholls1999 <azini@live.co.uk>
2022-01-19 15:11:35 +0000

    update styles for contact section

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import { initMap } from "./functions/map.js";
 import { Modal, toggleModal } from "./functions/modal.js";
 import { currentPageObserver } from "./functions/navigation/navigation.activePage.js";
 import { mobileMenu } from "./functions/navigation/navigation.mobile.js";
-
 initMap()
 mobileMenu()
-currentPageObserver()
+// currentPageObserver()
 Modal()
 toggleModal()
