571c027464d1671e7af417a71817ec932a820f21
alexnicholls1999 <azini@live.co.uk>
2022-02-11 13:43:23 +0000

    add changes to gallery and dimension modal links

--- src/functions/modal.js
+++ src/functions/modal.js
@@ -1,40 +1,41 @@
 const productsArr = [
-    {
-        title: "Pillars & Dining",
-        table: { 
-            row1Arr: ["Large Pillar", "15cm x 6cm", "£8 - £12"],
-            row2Arr: ["Sunflower", "10cm x 5cm", "£8"],
-            row3Arr: ["Wheatsheaf", "9.5cm x 4cm", "£10"],
-            row4Arr: ["Dining Taper", "2.5cm length", "£3.50"]
-        }
+  {
+    title: "Pillars & Dining",
+    table: {
+      row1Arr: ["Large Pillar", "15cm x 6cm", "£8 - £12"],
+      row2Arr: ["Sunflower", "10cm x 5cm", "£8"],
+      row3Arr: ["Wheatsheaf", "9.5cm x 4cm", "£10"],
+      row4Arr: ["Dining Taper", "2.5cm length", "£3.50"],
     },
-    {
-        title: "Large Votives",
-        table: {
-            row1Arr: ["Large Pillar", "15cm x 6cm", "£8 - £12"],
-            row2Arr: ["Sunflower", "10cm x 5cm", "£8"],
-            row3Arr: ["Wheatsheaf", "9.5cm x 4cm", "£10"],
-            row4Arr: ["Dining Taper", "2.5cm length", "£3.50"]
-        }
+  },
+  {
+    title: "Large Votives",
+    table: {
+      row1Arr: ["Large Pillar", "15cm x 6cm", "£8 - £12"],
+      row2Arr: ["Sunflower", "10cm x 5cm", "£8"],
+      row3Arr: ["Wheatsheaf", "9.5cm x 4cm", "£10"],
+      row4Arr: ["Dining Taper", "2.5cm length", "£3.50"],
     },
-    {
-        title: "Other",
-        table: {
-            row1Arr: ["Large Pillar", "15cm x 6cm", "£8 - £12"],
-            row2Arr: ["Sunflower", "10cm x 5cm", "£8"],
-            row3Arr: ["Wheatsheaf", "9.5cm x 4cm", "£10"],
-            row4Arr: ["Dining Taper", "2.5cm length", "£3.50"]
-        }
-    }
+  },
+  {
+    title: "Other",
+    table: {
+      row1Arr: ["Large Pillar", "15cm x 6cm", "£8 - £12"],
+      row2Arr: ["Sunflower", "10cm x 5cm", "£8"],
+      row3Arr: ["Wheatsheaf", "9.5cm x 4cm", "£10"],
+      row4Arr: ["Dining Taper", "2.5cm length", "£3.50"],
+    },
+  },
 ];
 
 export function renderProducts() {
-    const productContainer = document.querySelector(".products")
-    
-    const renderProductsTables = productsArr.map((product) => {
-        const { title, table } = product;
+  const productContainer = document.querySelector(".products");
 
-        return `
+  const renderProductsTables = productsArr
+    .map((product) => {
+      const { title, table } = product;
+
+      return `
             <h3>${title}</h3>
             <table class="products-table">
                 <thead>
@@ -45,22 +46,39 @@ export function renderProducts() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr class="secondary-row">${table.row1Arr.map((row) => {return `<td>${row}</td>`}).join(' ')}</tr>
-                    <tr>${table.row2Arr.map((row) => {return `<td>${row}</td>`}).join(' ')}</tr>
-                    <tr class="secondary-row">${table.row3Arr.map((row) => {return `<td>${row}</td>`}).join(' ')}</tr>
-                    <tr>${table.row4Arr.map((row) => {return `<td>${row}</td>`}).join(' ')}</tr>
+                    <tr class="secondary-row">${table.row1Arr
+                      .map((row) => {
+                        return `<td>${row}</td>`;
+                      })
+                      .join(" ")}</tr>
+                    <tr>${table.row2Arr
+                      .map((row) => {
+                        return `<td>${row}</td>`;
+                      })
+                      .join(" ")}</tr>
+                    <tr class="secondary-row">${table.row3Arr
+                      .map((row) => {
+                        return `<td>${row}</td>`;
+                      })
+                      .join(" ")}</tr>
+                    <tr>${table.row4Arr
+                      .map((row) => {
+                        return `<td>${row}</td>`;
+                      })
+                      .join(" ")}</tr>
                 </tbody>
             </table>
         `;
-    }).join(' ')
+    })
+    .join(" ");
 
-    productContainer.innerHTML = renderProductsTables
+  productContainer.innerHTML = renderProductsTables;
 }
 
-export function Modal(){
-    const modal = document.querySelector(".dimensions");
+export function Modal() {
+  const modal = document.querySelector(".dimensions");
 
-    modal.innerHTML = `
+  modal.innerHTML = `
         <div class="modal-wrapper">
             <div class="modal">
                 <div class="modal-container">
@@ -74,28 +92,34 @@ export function Modal(){
         </div>
     `;
 
-    renderProducts()
+  renderProducts();
 }
 
-export function toggleModal(){
-    const modalBtn = document.querySelector("[data-ondimensionsclick]");
-    const closeBtn = document.querySelector("[data-closemodalclick]");
-    const modalmobileBtn = document.querySelector("[data-ondimensionsmobileclick]");
-    const modalLink = document.querySelectorAll(".hyperlink");
-    const modal = document.querySelector(".modal-wrapper");
+export function toggleModal() {
+  const modalBtn = document.querySelector("[data-ondimensionsclick]");
+  const closeBtn = document.querySelector("[data-closemodalclick]");
+  const modalmobileBtn = document.querySelector(
+    "[data-ondimensionsmobileclick]"
+  );
+  const modal = document.querySelector(".modal-wrapper");
 
-    const openModal = (e) => {
-        e.preventDefault();
-        modal.classList.add("open-modal")
-    }
+  const hyperlinks = document.getElementsByClassName("hyperlink");
 
-    const closeModal = (e) => {
-        e.preventDefault();
-        modal.classList.remove("open-modal")
-    }
+  const openModal = (e) => {
+    e.preventDefault();
+    modal.classList.add("open-modal");
+  };
 
-    modalBtn.addEventListener("click", openModal);
-    modalmobileBtn.addEventListener("click", openModal);
-    modalLink.addEventListener("click", openModal);
-    closeBtn.addEventListener("click", closeModal);
+  const closeModal = (e) => {
+    e.preventDefault();
+    modal.classList.remove("open-modal");
+  };
+
+  for (let i = 0; i < hyperlinks.length; i++) {
+    hyperlinks[i].addEventListener("click", openModal);
+  }
+
+  modalBtn.addEventListener("click", openModal);
+  modalmobileBtn.addEventListener("click", openModal);
+  closeBtn.addEventListener("click", closeModal);
 }
