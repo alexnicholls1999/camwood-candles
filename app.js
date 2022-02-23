1478801d370cf19c722422183d83841e9cc25736
alexnicholls1999 <azini@live.co.uk>
2022-02-23 09:32:52 +0000

    add changes

--- app.js
+++ app.js
@@ -15,53 +15,51 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use("/", router);
 app.use("/src", express.static("src"));
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Servier is running on port` + port);
+app.listen(port, "0.0.0.0");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(index));
 });
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(index));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(notFound));
+});
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(notFound));
-// });
+app.post("/", (req, res) => {
+  console.log(req.body);
+  const contactOutput = `
+    <h1>Camwood Candles Contact Enquiry</h1>
+    <p>Hi Carol</p>
+    <p>${req.body.message}</p>
+    <p>Regards <br> ${req.body.name} </p>
+  `;
 
-// app.post("/", (req, res) => {
-//   console.log(req.body);
-//   const contactOutput = `
-//     <h1>Camwood Candles Contact Enquiry</h1>
-//     <p>Hi Carol</p>
-//     <p>${req.body.message}</p>
-//     <p>Regards <br> ${req.body.name} </p>
-//   `;
+  //   const transporter = nodemailer.createTransport({
+  //     service: "gmail",
+  //     auth: {
+  //       user: process.env.GMAIL_EMAIL_ADDRESS,
+  //       pass: process.env.GMAIL_PASSWORD,
+  //     },
+  //     tls: {
+  //       rejectUnauthorized: false,
+  //     },
+  //   });
 
-//   const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: process.env.GMAIL_EMAIL_ADDRESS,
-//       pass: process.env.GMAIL_PASSWORD,
-//     },
-//     tls: {
-//       rejectUnauthorized: false,
-//     },
-//   });
+  //   const mailOptions = {
+  //     from: req.body.email,
+  //     to: "mailertest1999@gmail.com",
+  //     cc: "azini@live.co.uk",
+  //     subject: `Message from ${req.body.email}`,
+  //     html: contactOutput,
+  //   };
 
-//   const mailOptions = {
-//     from: req.body.email,
-//     to: "mailertest1999@gmail.com",
-//     cc: "azini@live.co.uk",
-//     subject: `Message from ${req.body.email}`,
-//     html: contactOutput,
-//   };
-
-//   transporter.sendMail(mailOptions, (error, info) => {
-//     if (error) {
-//       console.log(error);
-//       res.send("error" + error);
-//     } else {
-//       console.log(`Email sent: ` + info.response);
-//       res.send("success");
-//     }
-//   });
-// });
+  //   transporter.sendMail(mailOptions, (error, info) => {
+  //     if (error) {
+  //       console.log(error);
+  //       res.send("error" + error);
+  //     } else {
+  //       console.log(`Email sent: ` + info.response);
+  //       res.send("success");
+  //     }
+  //   });
+});
