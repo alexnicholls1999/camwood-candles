9bc54f3b488a9f4e077ecc1995e23b03a57d8415
alexnicholls1999 <azini@live.co.uk>
2022-04-17 23:00:29 +0100

    add changes to v1.1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import { buttonToggle, contactMe, root } from "./functions/button.js";
-import { sendContactForm } from "./functions/contactForm.js";
-import { initMap } from "./functions/map.js";
-import { Modal, toggleModal } from "./functions/modal.js";
-import { currentPageObserver } from "./functions/navigation/navigation.activePage.js";
-import { mobileMenu } from "./functions/navigation/navigation.mobile.js";
+import { buttonToggle, contactMe, root } from './functions/button.js';
+import { Components } from './functions/components/components.js';
+import { sendContactForm } from './functions/contactForm.js';
+import { initMap } from './functions/map.js';
+import { Modal, toggleModal } from './functions/modal.js';
+import { currentPageObserver } from './functions/navigation/navigation.activePage.js';
+import { mobileMenu } from './functions/navigation/navigation.mobile.js';
 
 initMap();
 mobileMenu();
@@ -14,3 +15,4 @@ root();
 buttonToggle();
 sendContactForm();
 currentPageObserver();
+Components();
